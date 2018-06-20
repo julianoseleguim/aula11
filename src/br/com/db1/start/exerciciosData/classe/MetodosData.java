c6a4f7961e5be550831d5a69c4792cab1f5cc788
julianoseleguim <jseleguim@gmail.com>
2018-06-19 22:59:30 -0300

    Atualização dos exercicios de Data usando a biblioteca Joda-Time

--- src/br/com/db1/start/exerciciosData/classe/MetodosData.java
+++ src/br/com/db1/start/exerciciosData/classe/MetodosData.java
@@ -1,10 +1,27 @@
 package br.com.db1.start.exerciciosData.classe;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.DayOfWeek;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import org.joda.time.DateTime;
+import org.joda.time.Days;
+import org.joda.time.Hours;
+import org.joda.time.LocalDate;
+import org.joda.time.LocalTime;
+import org.joda.time.Months;
+import org.joda.time.Years;
 
 public class MetodosData {
 		
+	public Boolean verificaAnoBissexto(LocalDate data) {
+		DateTime dateTime = new DateTime();
+		return dateTime.withYear(data.getYear()).year().isLeap();		
+	}
+	
 	public Boolean verifcaDiaUtil(Date dataParametro) {
 		Calendar calendario = Calendar.getInstance();
 		calendario.setTime(dataParametro);
@@ -14,5 +31,63 @@ public class MetodosData {
 		}
 			return true;
 	}
+		
+	public Boolean verifcaDiaUtilJoda(LocalDate dataParametro) {		
+		if ((dataParametro.getDayOfWeek() == DayOfWeek.SATURDAY.getValue()) 
+				|| dataParametro.getDayOfWeek() == DayOfWeek.SUNDAY.getValue()) {
+			return false;			
+		}
+			return true;
+	}
+	
+	public Integer retornaIdade(LocalDate dataNascimento) {		
+		LocalDate dataAtual = LocalDate.now();		
+		Integer idade = dataAtual.getYear() - dataNascimento.getYear();
+		
+		if (dataAtual.getMonthOfYear() < dataNascimento.getMonthOfYear()) {
+			idade--;
+		} else {
+			if (dataAtual.getMonthOfYear() == dataNascimento.getMonthOfYear()
+					&& dataAtual.getDayOfMonth() < dataNascimento.getDayOfMonth()) {
+				idade--;
+			}
+		}
+		return idade;				
+	}
+	
+	public long quantidadeDiasEntreDuasDatas(String data1, String data2) {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	    long diff=0;		
+		
+	    try {
+			Date primeiraData = sdf.parse(data1); 
+		    Date segundaData  = sdf.parse(data2);
+		
+		    long diffInMillies = Math.abs(segundaData.getTime() - primeiraData.getTime());
+		    diff = TimeUnit.DAYS.convert(diffInMillies, TimeUnit.MILLISECONDS);		    
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}		
+		return diff;		
+	}
+	
+	public Integer quantidadeDiasEntreDuasDatasJoda(LocalDate data1, LocalDate data2) {	
+	   Days d = Days.daysBetween(data1, data2);
+	   return d.getDays();		
+	}
 
+	public Integer quantidadeMesEntreDuasDatasJoda(LocalDate data1, LocalDate data2) {	
+		Months m = Months.monthsBetween(data1, data2);
+		return m.getMonths();		
+	}
+	
+	public Integer quantidadeAnoEntreDuasDatasJoda(LocalDate data1, LocalDate data2) {	
+		Years y = Years.yearsBetween(data1, data2);		
+		return y.getYears();		
+	}
+	
+	public Integer quantidadeHoraJoda(LocalTime relogio1, LocalTime relogio2) {		
+		Hours h = Hours.hoursBetween(relogio1, relogio2);
+		return h.getHours();		
+	}
 }
--- src/br/com/db1/start/exerciciosData/teste/MetodosDataTeste.java
+++ src/br/com/db1/start/exerciciosData/teste/MetodosDataTeste.java
@@ -4,6 +4,10 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.joda.time.LocalDate;
+import org.joda.time.LocalTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -11,22 +15,82 @@ import br.com.db1.start.exerciciosData.classe.MetodosData;
 
 public class MetodosDataTeste {
 
+	DateTimeFormatter sdf = DateTimeFormat.forPattern("dd/MM/yyyy");
+	DateTimeFormatter dtf = DateTimeFormat.forPattern("dd/MM/yyyy HH:mm");
+	
+	DateTimeFormatter tmf = DateTimeFormat.forPattern("HH:mm");
+	
+	@Test
+	public void deveVerificarAnoBissexto() {				
+		MetodosData data = new MetodosData();			
+		LocalDate dataBissexto = LocalDate.parse("16/01/2017", sdf);
+		Assert.assertTrue(true == data.verificaAnoBissexto(dataBissexto));
+	}
+		
 	@Test
 	public void deveVerificarDiaUtil() {
-		
 		MetodosData data = new MetodosData();		
-		Assert.assertTrue(data.verifcaDiaUtil(new Date()));		
-		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		Date sabado;
+		Date dataQualquer;
 		
 		try {
-			sabado = sdf.parse("02/06/2018");
-			Assert.assertFalse(data.verifcaDiaUtil(sabado));
+			dataQualquer = sdf.parse("04/06/2018");
+			Assert.assertTrue(data.verifcaDiaUtil(dataQualquer));
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
-		
-		
 	}
+	
+	@Test
+	public void deveVerificarDiaUtilJoda() {		
+		MetodosData data = new MetodosData();		
+		LocalDate dataQualquer = LocalDate.parse("25/06/2018", sdf);
+		Assert.assertTrue(data.verifcaDiaUtilJoda(dataQualquer));			
+	}
+	
+	@Test
+	public void deveRetornarIdade() {				
+		MetodosData data = new MetodosData();		
+		LocalDate dataNascimento = LocalDate.parse("27/10/1995", sdf);
+		Assert.assertTrue(22 == data.retornaIdade(dataNascimento));	
+	}
+	
+	@Test
+	public void deveRetornarDiasEntreDuasData() {	
+		MetodosData data = new MetodosData();
+		Assert.assertTrue(27 == data.quantidadeDiasEntreDuasDatas("18/05/2018", "14/06/2018"));		
+	}
+	
+	@Test
+	public void deveRetornarDiasEntreDuasDataJoda() {	
+		MetodosData data = new MetodosData();
+		LocalDate data1 = LocalDate.parse("18/05/2015", sdf);
+		LocalDate data2 = LocalDate.parse("18/05/2016", sdf);		
+		Assert.assertTrue(366 == data.quantidadeDiasEntreDuasDatasJoda(data1, data2));				
+	}
+	
+	@Test
+	public void deveRetornarMesEntreDuasDataJoda() {	
+		MetodosData data = new MetodosData();
+		LocalDate data1 = LocalDate.parse("18/05/2018", sdf);
+		LocalDate data2 = LocalDate.parse("18/07/2018", sdf);		
+		Assert.assertTrue(2 == data.quantidadeMesEntreDuasDatasJoda(data1, data2));				
+	}
+	
+	@Test
+	public void deveRetornarAnoEntreDuasDataJoda() {	
+		MetodosData data = new MetodosData();
+		LocalDate data1 = LocalDate.parse("18/05/2018", sdf);
+		LocalDate data2 = LocalDate.parse("18/07/2019", sdf);		
+		Assert.assertTrue(1 == data.quantidadeAnoEntreDuasDatasJoda(data1, data2));				
+	}
+	
+	@Test
+	public void deveRetornarQuantidadeHoraJoda() {	
+		MetodosData data = new MetodosData();
+		LocalTime relogio1 = LocalTime.parse("12:00", tmf);
+		LocalTime relogio2 = LocalTime.parse("14:00", tmf);
+		Assert.assertTrue(2 == data.quantidadeHoraJoda(relogio1, relogio2));				
+	}
+
 }
